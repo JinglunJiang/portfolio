256bb69b134946889adf75fd0af8510b2ca33df9
Jinglun Jiang <jinglunjiang@gmail.com>
2023-11-11 00:49:37 -0600

    added the like button

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let likeCount = localStorage.getItem('likeCount') || 0;
+
+function incrementLikes() {
+  likeCount++;
+  document.getElementById('like-count').innerText = likeCount;
+  localStorage.setItem('likeCount', likeCount);
+}
+
+// Set initial like count on page load
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById('like-count').innerText = likeCount;
+});
